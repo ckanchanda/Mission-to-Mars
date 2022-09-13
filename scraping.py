b3399e8eb3622b9b07515e4043818ae82df844d2
unknown <ckanchanda@gmail.com>
2022-09-13 13:28:02 -0600

    update

--- scraping.py
+++ scraping.py
@@ -135,6 +135,23 @@ def hemispheres(browser):
         browser.back()
     return hemispheres
 
+def scrape_hemisphere(html_text):
+    # parse html text
+    hemi_soup = soup(html_text, "html.parser")
+    # adding try/except for error handling
+    try:
+        title_elem = hemi_soup.find("h2", class_="title").get_text()
+        sample_elem = hemi_soup.find("a", text="Sample").get("href")
+    except AttributeError:
+        # Image error will return None, for better front-end handling
+        title_elem = None
+        sample_elem = None
+    hemispheres = {
+        "title": title_elem,
+        "img_url": sample_elem
+    }
+    return 
+
 if __name__ == "__main__":
     
     # If running as script, print scraped data
